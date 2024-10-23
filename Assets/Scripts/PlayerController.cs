52a4d4c4ccc53eba442accef74a867679800d800
Daniel <daniel10gja@gmail.com>
2024-10-22 23:08:27 -0300

    fix: jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,16 +28,16 @@ public class PlayerController : MonoBehaviour
         }
 
         //transform.position = Vector3.right * runSpeed * Time.deltaTime + transform.position;
-        //rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
-
+    
         if(jumpCount == 2) {
             canJump = false;
         }
 
-        if (Input.GetKeyDown("space") && canJump && !isGameOver) {
-            rb2d.velocity = Vector3.up * 7.5f;
+        if (Input.GetButtonDown("Jump") && canJump && !isGameOver) {
+            rb2d.velocity = Vector2.up * 7.5f;
             anim.SetTrigger("jump");
             jumpCount += 1;
+            Debug.Log(jumpCount);
         }
     }
 
@@ -45,26 +45,21 @@ public class PlayerController : MonoBehaviour
         isGameOver = true;
         anim.SetTrigger("death");
         StopCoroutine("IncreaseGameSpeed");
-
         StartCoroutine("ShowGameOverPanel");
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.CompareTag("Ground")) {
-            jumpCount = 0;
-            canJump = true;
-        }
-        if(collision.gameObject.CompareTag("Obstacle")) {
-            GameOver();
-        }
-        if(collision.gameObject.CompareTag("BottomDetector")) {
-            GameOver();
+        jumpCount = 0;
+        canJump = true;
+        }else if(collision.gameObject.CompareTag("Obstacle") || collision.gameObject.CompareTag("BottomDetector")) {
+        GameOver();
         }
     }
 
     IEnumerator IncreaseGameSpeed() {
         while(true) {
-             yield return new WaitForSeconds(10);
+            yield return new WaitForSeconds(10);
 
             if(runSpeed < 16){
                 runSpeed += 0.2f;
@@ -89,7 +84,7 @@ public class PlayerController : MonoBehaviour
     public void JumpButton(){
         if (canJump && !isGameOver)
         {
-            rb2d.velocity = Vector3.up * 7.5f;
+            rb2d.velocity = Vector2.up * 7.5f;
             anim.SetTrigger("jump");
             jumpCount += 1;
         }
