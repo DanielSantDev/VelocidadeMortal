6b6351edaff054842d9d221f4804dbd268570961
Daniel Santana <64343433+DanielSantDev@users.noreply.github.com>
2024-11-04 15:30:55 -0300

    Update GameOverPanelScript.cs

--- Assets/Scripts/GameOverPanelScript.cs
+++ Assets/Scripts/GameOverPanelScript.cs
@@ -23,20 +23,7 @@ public class GameOverPanelScript : MonoBehaviour
     public void LoadSampleMenu()
     {
         SceneManager.LoadScene("Assets/Scenes/SampleScene.unity");
+        PlayerPrefs.SetInt("SceneLoaded", 0);
     }
 
-    public void LoadMediumModeScene()
-    {
-        SceneManager.LoadScene("Assets/Scenes/LoadMediumModeScene.unity");
-    }
-
-    public void LoadHardModeScene()
-    {
-        SceneManager.LoadScene("Assets/Scenes/LoadHardModeScene.unity");
-    }
-
-    public void LoadImpossibleModeScene()
-    {
-        SceneManager.LoadScene("Assets/Scenes/LoadImpossibleModeScene.unity");
-    }
 }
