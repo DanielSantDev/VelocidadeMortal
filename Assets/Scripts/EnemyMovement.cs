e274680de42b607dc57af32f930909efc72cd9aa
Daniel <daniel10gja@gmail.com>
2024-11-06 20:09:17 -0300

    feat: enemy movement script

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+}
